61a42c9a5c3d5fc366b39e0363b796a473a106e7
Profsain <profsaincrown@gmail.com>
2021-12-23 23:29:41 -0800

    Added eslint code

--- models/person.js
+++ models/person.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose')
 const uniqueValidator = require('mongoose-unique-validator')
 
-const url = process.env.MONGODB_URI
+const url = process.env.MONGODB_URL
 
 console.log('Connecting to Person MongoDB URL', url)
 
